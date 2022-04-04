2f1b35e43fccdfa1ecc3562552869fb012fa969d
conorbros <conor.brosnan@instaclustr.com>
2022-04-04 22:06:26 +1000

    change artefact name

--- scripts/build_release.sh
+++ scripts/build_release.sh
@@ -6,15 +6,12 @@ SCRIPT_DIR="$(cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 cd $SCRIPT_DIR/..
 
-cargo test --release
 cargo build --release
 
-# Make glibc version
-mkdir -p latte
 cp target/release/latte latte
 
 # extract the crate version from Cargo.toml
 CRATE_VERSION="$(cargo metadata --format-version 1 --offline --no-deps | jq -c -M -r '.packages[] | select(.name == "latte") | .version')"
-tar -cvzf latte-linux_amd64-${CRATE_VERSION}.tar.gz latte/latte
+tar -cvzf latte_${CRATE_VERSION}_amd64.tar.gz latte
 
 rm -rf latte
